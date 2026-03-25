a866053e5f7fbd262d627acac136c6b91d0bac0c
Ucenik <Ucenik@DESKTOP-A6MV7AL>
2026-03-25 19:38:43 +0100

    0.7

--- poligon.cs
+++ poligon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,8 +36,33 @@ namespace poligon2026B
             Console.WriteLine("Poligon sa {0} temena: ", br_temena);
             for (int i = 0; i < br_temena; i++)
             {
-                Console.WriteLine("teme #{0} ({1},{2})", i, teme[i].x, teme[i].y);
+                Console.WriteLine("teme #{0} ({1},{2})", i + 1, teme[i].x, teme[i].y);
             }
+        }
+        public void snimi()
+        {
+            StreamWriter sw = new StreamWriter("poligon.txt");
+            sw.WriteLine(br_temena);
+            for (int i = 0; i < br_temena; i++)
+            {
+                sw.WriteLine(teme[i].x);
+                sw.WriteLine(teme[i].y);
+            }
+            sw.Close();
+        }
+        static public poligon ucitaj()
+        {
+            StreamReader sr = new StreamReader("poligon.txt");
+            int br = Convert.ToInt32(sr.ReadLine());
+            poligon novi = new poligon(br);
+            for (int i = 0; i < br; i++)
+            {
+                novi.teme[i] = new tacka();
+                novi.teme[i].x = Convert.ToDouble(sr.ReadLine());
+                novi.teme[i].y = Convert.ToDouble(sr.ReadLine());
+            }
+            sr.Close();
+            return novi;
         }
     }
 }
